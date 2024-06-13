02fcf4454c3d9124ec538ab203a463e9ff41d221
Adam Lenssa <adam.lenssa@gmail.com>
2024-06-13 12:55:00 -0400

    Refactored

--- part1/SQL.sql
+++ part1/SQL.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    motto TEXT
+    motto TEXT NULL
 );
 
 CREATE TABLE dogs (
@@ -13,21 +13,27 @@ CREATE TABLE dogs (
 );
 
 CREATE TABLE favorites (
-    user_id INTEGER,
-    dog_id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    dog_id INTEGER NOT NULL,
+    UNIQUE (user_id, dog_id),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(dog_id) REFERENCES dogs(id)
 );
 
 INSERT INTO users (first_name, last_name, motto)
 VALUES ('Jon', 'Higgz', 'I love coding'),
-        ('Andrey', 'Rusterton', 'I love coding even more'),
-        ('Peter', 'Garboni', 'I love coding even more');
+       ('Andrey', 'Rusterton', 'I love coding even more'),
+       ('Peter', 'Garboni', 'I love coding even more');
 
 INSERT into dogs (name) 
 VALUES ('DOOMSLAYER'),
-('Zoey'),
-('Jefferey');
+       ('Zoey'),
+       ('Jefferey');
 
 INSERT INTO favorites (user_id, dog_id) 
-VALUES (3, 1), (1, 1), (2, 1), (1, 3), (2, 2)
+VALUES (3, 1), 
+       (1, 1), 
+       (2, 1), 
+       (1, 3), 
+       (2, 2);
--- part2/d.sql
+++ part2/d.sql
@@ -1,7 +1,7 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select u.first_name, u.last_name
-from favorites f 
-JOIN users u ON u.id = f.user_id
-WHERE f.dog_id = 2
+SELECT first_name, last_name
+from favorites 
+JOIN users ON users.id = favorites.user_id
+WHERE favorites.dog_id = 2
 ;
